d5366d32fc18f0ca810bae31e352fdccc6123412
jvsguerra <jvsguerra@gmail.com>
2025-09-23 17:35:09 -0300

    Update a build and Lmod configuration based on Bruno Focassio (LNNano) recipes

--- scipion/3.8.1/build.sh
+++ scipion/3.8.1/build.sh
@@ -7,29 +7,11 @@
 # João V S Guerra joao.guerra@lnbio.cnpem.br
 # Jose G C Pereira jose.pereira@lnbio.cnpem.br
 
-#### USER CONFIGURABLE VARIABLES
-###
-###
-
-# Available flavours are: base, spa
-# You can build multiple containers by adding them separated by a space
-# For example, CONTAINER_FLAVOURS="base spa"
-# Remember that you will need base for any container
-CONTAINER_FLAVOURS="base full"
-
-### CLUSTER SPECIFIC
-# You can add your cluster-specific commands here
-#PREPARE_ENV="module load XXX"
-PREPARE_SCREEN="xhost +"
-CLUSTER_PREP="$PREPARE_ENV $PREPARE_SCREEN"
-$CLUSTER_PREP
-
-###
-###
-#### END OF USER CONFIGURABLE VARIABLES
+VERSION=$(basename "$(pwd)")
+TARGET="scipion-${VERSION}"
 
 echo "Preparing to build Scipion Apptainer image locally..."
-echo "System will build $CONTAINER_FLAVOURS"
+echo "Building $TARGET.sif"
 
 # Check if apptainer or singularity is available
 if command -v apptainer &> /dev/null; then
@@ -41,11 +23,7 @@ else
     exit 1
 fi
 
-for F in $CONTAINER_FLAVOURS; do
-    TARGET="scipion-$F"
-    echo "Compiling $TARGET image..."
-    echo "Result will be in ./$TARGET.sif"
-    APPTAINERENV_DISPLAY=$DISPLAY $CONTAINER_CMD build --nv --force ./$TARGET.sif ./apptainer/$TARGET.def
-done
+# APPTAINERENV_DISPLAY=$DISPLAY 
+$CONTAINER_CMD build --nv --force ./$TARGET.sif ./apptainer/$TARGET.def
 
 echo "Finished."
